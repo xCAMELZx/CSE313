6a2c4414a4df2a5e68844422e3e9cc2b03567a52
Yousef Jarrar <005845836@coyote.csusb.edu>
2018-10-29 10:27:25 -0700

    Update

--- Lab3/DaysOfWeek.asm
+++ Lab3/DaysOfWeek.asm
@@ -1,32 +1,44 @@
-; CLASS: CSE 313 MACHINE ORGANIZATION 
+; CLASS: CSE 313 MACHINE ORGANIZATION
+; INSTRUCTOR: TALINE GEORGIOU
 ; TERM: FALL 2018 
 ; NAME(S): YOUSEF JARRAR, AUDREY CARDS 
 ; LAB #3 - DAYS OF THE WEEK 
 ; DESCRIPTION: The purpose of this lab is to take the user's input and output the corresponding day. 
 ; They will have the ability to select all 7 days. Formatted in LC3 as 0-6. 0 is Sunday; 6 is Saturday. 
 ; If the input is other than defined #'s. It will not output anything. 
+; Version 2 -- The one originally submitted DID NOT LOOP
 
 	.ORIG x3100
-	LEA R0, PROMPT
+RESTART	LEA R0, PROMPT
 	PUTS	;Outputs the message "Enter a Number"  
-	GETC	;User input, # inputted 
-	ADD R3, R0, #0 
+	GETC	;User input, # inputted
+ 
+	ADD R3, R0, x0 
 	ADD R3, R3, #-16
 	ADD R3, R3, #-16
 	ADD R3, R3, #-16  ;Subtracted -16 (3 times).. We can only add 5 bits at a time 
-	LEA R0, DAYS
-	ADD R3, R3, #0 
 
-LOOP
-	BRz DISPLAY
+	AND R6, R6, #0 	  ; LOAD R6 WITH 0 
+	ADD R6, R6, R3	  ; LOAD CONTENT OF R3 INTO R6 
+	ADD R6, R6, #-6   ; R6 + (-6) = LOADS INTO R6
+
+	BRp EXIT          ; IF CONDITION IS TRUE THEN HALT
+
+	LEA R0, DAYS
+	ADD R3, R3, x0 
+
+
+LOOP	BRz DISPLAY
 	ADD R0, R0, #10
 	ADD R3, R3, #-1
 	BR LOOP 
 	
 DISPLAY PUTS 
-	HALT 
+	BR RESTART 
 
-	PROMPT .STRINGZ " Please enter the number: " 
+EXIT	HALT 
+
+PROMPT .STRINGZ " Please enter the number: " 
 
 	DAYS
 	 .STRINGZ "Sunday   " 
---
+++ Lab4/FibonacciNumbers.asm
@@ -0,0 +1,83 @@
+;CLASS: CSE 313 - MACHINE ORGANIZATION
+;INSTRUCTOR: TALINE GEORGIOU
+;TERM: FALL 2018
+;NAME: YOUSEF JARRAR, AUDREY CARDS
+;LAB 4 - FIBONACCI NUMBERS
+;DESCRIPTION: THIS PROGRAM WILL CALCULATE FIBONACCI NUMBER PROVIDED
+; IT WILL ALSO LOCATE THE HIGHEST FIBONACCI NUMBER (Fn, n-th Fibonacci #)
+; The program will locate the largest Fn so no overflow happens, Takes Fn
+; N, and FN. The "n" value lis located in the data file. 
+
+	.ORIG x3000
+
+	LDI R1, INPUT
+	AND R2, R2, #0
+	ADD R2, R1, #-2
+	BRn X
+	BRz Y
+
+	BRp MORE
+	X STI R1, F
+
+	BR END
+
+	Y ADD R1, R1, #-1
+	STI R1, F
+
+	BR END
+
+	MORE  AND R3, R3, #0
+	ADD R3, R3, #1
+	AND R4, R4, #0
+	ADD R4, R4, #1
+	AND R5, R5, #0
+	AND R6, R6, #0
+	ADD R6, R6, R1
+	ADD R6, R6 ,#-2
+
+	LOOP ADD R5, R3, R4 ;F = R3 + R4 which is == (X + Y )
+
+	AND R3, R3, #0
+	ADD R3, R3, R4
+	AND R4, R4, #0
+	ADD R4, R4, R5
+	ADD R6, R6, #-1
+	BRp LOOP
+
+	STI R5, F
+
+	END AND R3, R3, #0
+
+	ADD R3, R3, #1
+	AND R4, R4, #0
+	ADD R4, R4, #1
+	AND R2, R2, #0
+	ADD R2, R2, #2
+	AND R5, R5, #0
+
+	LOOP2 ADD R5, R3, R4 ;R5 = R3 + R4 (Fn = a + b)
+
+	BRn STORES
+	AND R3, R3, #0
+	ADD R3, R3, R4
+	AND R4, R4, #0
+	ADD R4, R4, R5
+	ADD R2, R2 ,#1
+
+	BR LOOP2
+
+	STORES NOT R3, R3
+	ADD R3, R3, #1
+	ADD R5, R5, R3
+
+	STI R5, Fn
+	STI R2, N
+	HALT
+
+	INPUT	.FILL x3100
+	F		.FILL x3101
+	N		.FILL X3102
+	Fn		.FILL x3103
+
+
+.END
